43a57b7cfae3dd110240a4fcbb47d466b495f18f
loudingjia <1173691363@qq.com>
2020-12-15 12:42:21 +0800

    1541

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
+//1541
 #include <stdio.h>
-
+#include <math.h>
 int main() {
-    printf("Hello GitHub!!!!\n");
+    unsigned int n;
+    int num = 0;
+    while(scanf("%d", &n), n) {
+        int i = 0;
+        while (i * i * i < n) {
+            ++i;
+            if(i * i * i == n) {
+                ++num;
+                break;
+            }
+        }
+    }
+    printf("%d\n", num);
     return 0;
 }
